d7de3e8fee8052f1fb0278567718d91f3c5dcf27
Vinicios <vorceek@gmail.com>
2025-09-01 20:44:16 -0300

    Add Configs: Prettier and ESLint.

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
